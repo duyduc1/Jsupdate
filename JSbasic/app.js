ff3b79bb71cd3e5c91d33596940ce9c389c1a217
duyduc1 <106147757+duyduc1@users.noreply.github.com>
2023-12-15 17:11:23 +0700

    basicJS

---
+++ JSbasic/app.js
@@ -0,0 +1,122 @@
+// var name = 'Vũ Duy Đức'
+
+// console.log(name); //in ra tên
+// alert(name) in ra thông báo
+
+// confirm('Đây là thông báo')
+
+// prompt("Đây là thô?ng báo")// cho phép nhập thông báo
+// setTimeout(function(){
+//     console.log(name);
+// },1000)
+
+// setInterval(function(){
+//     console.log("Đây là thông báo" + Math.random());
+// },2000) in ra thông báo nhiều lần
+
+
+// vid 2 
+// var name = 'Vũ'
+// var lastName = "Duy"
+// var firstName = "Đức"
+
+// var fullName = name + " " + lastName + " " + firstName;
+
+// console.log(fullName);
+
+
+// const a = 2 ; 
+// const b = 3 ;
+
+// if(a > 0 && b > 0 ){
+//     console.log("Điều kiện đúng");
+// }else{
+//     console.log("Điều kiện sai");
+// }
+
+
+// vid 3 : toán tử học 
+
+// var a = 8 ;
+// a--;
+// a--;
+
+
+// console.log(a);
+
+
+//vid 5 
+// var a = 7 
+
+// a += 2
+// a -= 2
+// a *= 2
+// a /= 2
+// console.log(a);
+
+
+
+//vid 6 
+
+// var FistName = 'Đức'
+// var midName = 'Duy'
+// var lastName = 'Vũ'
+
+// console.log(FistName + " " + midName + " " + lastName);
+
+// FistName += 'Duy'
+// console.log(FistName);
+
+
+// var a = 1 
+// var b = 2 
+
+// if(a > b ){
+//             console.log("Điều kiện đúng");
+// }else{
+//             console.log("Điều kiện sai");
+// } có thể so sánh tên nếu như cả hai biến tên trùng nhau
+
+//vid 7 dữ liệu boolean (đúng hoặc sai)
+
+// var a = 1 
+// var b = 2
+
+// var isSuccess = a > b 
+// console.log(isSuccess);
+
+// var isSuccess = 1 < 2 ;
+
+// if(isSuccess){
+//         console.log("Điều kiện đúng");
+// }else{
+//         console.log("Điều kiên sai");
+// }
+
+
+// toán tử logical
+
+// var a = 1 
+// var b = 2 
+// var c = 3 
+
+// if(a > 0 && b > 0  && c > 0){
+//     console.log("Điều kiện đúng");
+// }else{
+//     console.log("Điều kiện sai");
+// }
+
+// if(a < 0 || b > 0  || c < 0){
+//     console.log("Điều kiện đúng");
+// }else{
+//     console.log("Điều kiện sai");
+// } // trường hợp này điều kiện vẫn đúng vì có || vẫn so sáng được b > 0 
+
+// if(!(a<0)){
+//     console.log("Điều kiện vẫn đúng");
+// } Điều kiện vẫn đúng vì phủ định của a < 0 
+
+
+
+//vid 9 
+// các kiểu dữ liệu trong js Số : int , String : Mãng , Boolean : Đúng sai , Null : không có gì , Undifined : không tìm thấy , 
